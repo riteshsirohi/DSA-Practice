bec87489421c990d24d32a1b9494607e06f66782
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-07-20 09:46:57 +0530

    [71] [Time Beats: 86.83%] [Memory Beats: 68.12%] - LeetPush

---
+++ Strings/71-Simplify-Path.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<Character> st = new Stack<>();
+        char[] arr = path.toCharArray();
+
+        for(int i:arr){
+            st.push
+        }
+
+
+    }
+}
