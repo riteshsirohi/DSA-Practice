954bb6cc1eff899411c33ce3607578f665040d59
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-07-22 13:11:30 +0530

    [130] [Time Beats: 99.68%] [Memory Beats: 80.65%] - LeetPush

---
+++ Matrix/130-Surrounded-Regions.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+
+        // Step 1: Traverse the border and mark 'O's and connected 'O's as 'T' (temporary)
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                dfs(board, i, n - 1);
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[m - 1][j] == 'O') {
+                dfs(board, m - 1, j);
+            }
+        }
+
+        // Step 2: Mark remaining 'O's as 'X' and restore 'T's to 'O'
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';  // Surrounded 'O'
+                } else if (board[i][j] == 'T') {
+                    board[i][j] = 'O';  // Restore 'T' to 'O'
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int row, int col) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != 'O') {
+            return;
+        }
+
+        board[row][col] = 'T';  // Mark the current 'O' as 'T'
+
+        // Explore adjacent cells
+        dfs(board, row - 1, col);
+        dfs(board, row + 1, col);
+        dfs(board, row, col - 1);
+        dfs(board, row, col + 1);
+    }
+}
