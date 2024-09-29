7a6fd5986745de91dca35b2c31529628e850092f
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-09-29 10:48:13 +0530

    [724] [Time Beats: 94.12%] [Memory Beats: 13.24%] - LeetPush

---
+++ Arrays/724-Find-Pivot-Index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+      int[] pre = new int[nums.length];
+      int sum = nums[0];
+      pre[0]=0;
+
+      for(int i=1;i<nums.length;i++){
+        sum += nums[i];
+          pre[i] = nums[i-1] + pre[i-1];
+      }
+      for(int i=0;i<nums.length;i++){
+        if(pre[i] == sum-nums[i]-pre[i]){
+            return i;
+        }
+      }  
+      return -1;
+    }
+}
