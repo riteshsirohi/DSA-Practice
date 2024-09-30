3d92d7ac037875af77634160601bc8d58879ac5d
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-09-30 16:05:08 +0530

    [118] [Time Beats: 84.00%] [Memory Beats: 79.52%] - LeetPush

---
+++ Arrays/118-Pascal's-Triangle.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>(numRows);
+        
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>(i + 1);
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(row);
+        }
+        
+        return result;
+    }
+}
