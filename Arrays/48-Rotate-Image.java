8b9e3a356f230fa134ed599121a0d97754e9fcbf
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-10-07 16:04:12 +0530

    [48] [Time Beats: 100.00%] [Memory Beats: 27.85%] - LeetPush

---
+++ Arrays/48-Rotate-Image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            reverserow(matrix[i]);
+        }
+    }
+
+    private void reverserow(int[] row) {
+        int left = 0;
+        int right = row.length - 1;
+        while (left < right) {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
