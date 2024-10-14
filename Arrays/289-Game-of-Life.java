aeddfc9d3bb7d16b819c1130c56b346d813ebe92
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-10-14 11:47:53 +0530

    [289] [Time Beats: 100.00%] [Memory Beats: 74.06%] - LeetPush

--- Arrays/289-Game-of-Life.java
+++ Arrays/289-Game-of-Life.java
@@ -1,4 +1,19 @@
 class Solution {
+    private int solve(int i, int j, int row, int col, int[][] board){
+      int live = 0;
+
+      if(i+1<row && board[i+1][j] == 1 ) live++;
+      if(j+1<col && board[i][j+1] == 1 ) live++;
+      if(i-1>=0 && board[i-1][j] == 1 ) live++;
+      if(j-1>=0 && board[i][j-1] == 1 ) live++;
+      if(i+1 <row && j+1<col && board[i+1][j+1] == 1 ) live++;
+      if(i-1>=0 && j-1>=0 && board[i-1][j-1] == 1 ) live++;
+      if(i+1<row && j-1>=0 && board[i+1][j-1] == 1 ) live++;
+      if(i-1>=0 && j+1<col && board[i-1][j+1] == 1 ) live++;
+       
+       return live;
+    }
+
     public void gameOfLife(int[][] board) {
         int row = board.length;
         int col = board[0].length;
@@ -11,7 +26,7 @@ class Solution {
         }
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                int live = findlive(i, j, clone, row, col);
+                int live = solve(i, j, row, col, clone);
                 if (clone[i][j] == 1) {
                     if (live < 2 || live > 3) {
                         board[i][j] = 0;
@@ -25,18 +40,5 @@ class Solution {
                 }
             }
         }
-    }
-
-    int findlive(int i, int j, int[][] board, int row, int col) {
-        int live = 0;
-        if (i + 1 < row && board[i + 1][j] == 1) live++;
-        if (j + 1 < col && board[i][j + 1] == 1) live++;
-        if (j - 1 >= 0 && board[i][j - 1] == 1) live++;
-        if (i - 1 >= 0 && board[i - 1][j] == 1) live++;
-        if (i + 1 < row && j + 1 < col && board[i + 1][j + 1] == 1) live++;
-        if (i + 1 < row && j - 1 >= 0 && board[i + 1][j - 1] == 1) live++;
-        if (i - 1 >= 0 && j + 1 < col && board[i - 1][j + 1] == 1) live++;
-        if (i - 1 >= 0 && j - 1 >= 0 && board[i - 1][j - 1] == 1) live++;
-        return live;
     }
 }
