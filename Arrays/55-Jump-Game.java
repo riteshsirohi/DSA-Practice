4ecd70b65a06b05d717116a72d72975298f062bc
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-10-23 10:20:34 +0530

    [55] [Time Beats: 49.73%] [Memory Beats: 32.38%] - LeetPush

--- Arrays/55-Jump-Game.java
+++ Arrays/55-Jump-Game.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach = 0;  
-        
-        for (int i = 0; i < nums.length; i++) {
-            
-            if (i > maxReach) {
-                return false;
-            } 
-           
-            maxReach = Math.max(maxReach, i + nums[i]);
-            
-            if (maxReach >= nums.length - 1) {
+        int n = nums.length;
+        if(n==1) return true;
+
+         int left = 0;
+         int right = 0;
+
+         while(right<n-1){
+            int farther = 0;
+
+            for(int i=left;i<=right;i++){
+                farther = Math.max(farther, i+nums[i]);
+            }
+            if(farther >= n-1){
                 return true;
             }
-        }
-        
-    
+            left = right + 1;
+            right = farther;
+
+            if (left > right) {
+                return false;
+            }
+         }
         return false;
     }
 }
