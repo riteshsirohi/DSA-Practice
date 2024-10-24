197cc20ecde9d6226ffd719a1da870fa13d2a1ae
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-10-24 09:31:05 +0530

    [84] [Time Beats: 16.14%] [Memory Beats: 16.85%] - LeetPush

--- Arrays/84-Largest-Rectangle-in-Histogram.java
+++ Arrays/84-Largest-Rectangle-in-Histogram.java
@@ -1,59 +1,56 @@
 class Solution {
-    private int[] NSR(int[] arr){
-        int[] res= new int[arr.length];
-        Stack<Integer> st = new Stack<>();
-        int Rend=arr.length;
-
-        for(int i=arr.length-1;i>=0;i--){
-                while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
-                    st.pop();
-                }
-                if(!st.isEmpty()){
-                    res[i]=st.peek();
-                }else{
-                    res[i]=Rend;
-                }
-            st.push(i);
-            }
-        return res;
-        }
     private int[] NSL(int[] arr){
-        int[] res= new int[arr.length];
+        int[] res = new int[arr.length];
         Stack<Integer> st = new Stack<>();
-        int Lend=-1;
-
         for(int i=0;i<arr.length;i++){
-                while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
-                    st.pop();
-                }
-                if(!st.isEmpty()){
-                    res[i]=st.peek();
-                }else{
-                    res[i]=Lend;
-                }
-            st.push(i);
-            }
-        return res;
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+            st.pop();
         }
+        if(st.isEmpty()){
+           res[i] = -1;
+        }else{
+            res[i] = st.peek();
+        }
+        st.push(i);
+      } 
+      return res;
+    }
+
+    private int[] NSR(int[] arr){
+        int[] res = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
+           while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+            st.pop();
+           }
+           if(st.isEmpty()){
+            res[i] = arr.length;
+           }else{
+            res[i] = st.peek();
+           }
+           st.push(i);
+        }
+        return res;
+    }
 
     public int largestRectangleArea(int[] heights) {
-        int n=heights.length;
-        
-        int[] nsl = NSL(heights);
-        int[] nsr=  NSR(heights);
+        int max = Integer.MIN_VALUE;
+        int n = heights.length;
+
+        int[] NSr = NSR(heights);
+        int[] NSl = NSL(heights);
 
         int[] width = new int[n];
-        int[] area= new int[n];
-        int max=Integer.MIN_VALUE;
+        int[] area = new int[n];
 
         for(int i=0;i<n;i++){
-           width[i]=nsr[i]-nsl[i]-1;
+            width[i] = NSr[i]-NSl[i]-1;
         }
         for(int i=0;i<n;i++){
-           area[i]=heights[i]*width[i];
-           if(max<area[i]){
-               max=area[i];
-           }
+            area[i] = heights[i] * width[i];
+            if(max<area[i]){
+                max = area[i];
+            }
         }
         return max;
     }
