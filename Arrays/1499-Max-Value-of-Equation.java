9238c5c652994a7c17f171a63572f054290bee4b
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-10-24 15:53:49 +0530

    [1499] [Time Beats: 94.88%] [Memory Beats: 98.80%] - LeetPush

--- Arrays/1499-Max-Value-of-Equation.java
+++ Arrays/1499-Max-Value-of-Equation.java
@@ -1,19 +1,19 @@
 class Solution {
     public int findMaxValueOfEquation(int[][] points, int k) {
-       int n = points.length;
-        int max = Integer.MIN_VALUE;
-        int flag=1;
+        int n = points.length;
+        int max = Integer.MIN_VALUE; 
+        int flag = 1;
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             if(flag<i+1) flag = i+1;
-            for(int j=flag;j<n;j++){
-                int val = Math.abs(points[i][0]-points[j][0]);
-                if(val<=k){
-                 int eq = points[i][1]+points[j][1]+val;
-                 if(eq > max){
-                        max = eq;
-                        flag = j;
-                    }
+            for (int j = flag; j < n; j++) { 
+                int val = Math.abs(points[i][0] - points[j][0]); 
+                if (val <= k) {
+                    int ans = points[i][1] + points[j][1] + val; 
+                   if(ans>max){
+                    max = ans;
+                    flag = j;
+                   }
                 }else{
                     break;
                 }
