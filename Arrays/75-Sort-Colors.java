58f29ed438b8a8c6d414b5400f7f861191254aba
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-10-25 10:12:25 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 48.51%] - LeetPush

--- Arrays/75-Sort-Colors.java
+++ Arrays/75-Sort-Colors.java
@@ -1,26 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zero=0;
-        int one=0;
-        int two=0;
+      int zero = 0;
+      int one = 0;
+      int two = 0;
 
-        for(int i:nums){
-            if(i==0){
-                zero++;
-            }else if(i==1){
-                one++;
-            }else if(i==2){
-                two++;
-            }
+      for(int i=0;i<nums.length;i++){
+        if(nums[i]==0){
+            zero++;
+        }else if(nums[i]==1){
+            one++;
+        }else{
+            two++;
         }
-        for(int i=0;i<zero;i++){
-            nums[i]=0;
-        }
-        for(int i=zero;i<one+zero;i++){
-            nums[i]=1;
-        }
-        for(int i=one+zero;i<zero+one+two;i++){
-            nums[i]=2;
-        }
+      }
+      for(int i=0;i<zero;i++){
+        nums[i] = 0;
+      }
+      for(int i= zero;i<zero+one;i++){
+        nums[i] = 1;
+      }
+      for(int i= zero+one; i<zero+one+two;i++){
+        nums[i] = 2;
+      }
     }
 }
