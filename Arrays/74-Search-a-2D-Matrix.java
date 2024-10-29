f31804255d09423aea4fb614ec23eadc3327b855
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-10-29 09:57:35 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 11.72%] - LeetPush

---
+++ Arrays/74-Search-a-2D-Matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+       int row = 0;
+       int col = n-1;
+
+       while(row<m && col>=0){
+        int val = matrix[row][col];
+
+        if(val==target){
+            return true;
+        }
+        if(val>target){
+            col--;
+        }else{
+            row++;
+        }
+       }
+         return false;
+    }
+}
