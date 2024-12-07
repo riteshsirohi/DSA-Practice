8363d77c333e564b438672d5f32b0127451cfa31
riteshsirohi <105536105+riteshsirohi@users.noreply.github.com>
2024-12-07 11:21:01 +0530

    [215] [Time Beats: 16.04%] [Memory Beats: 65.92%] - LeetPush

---
+++ Arrays/215-Kth-Largest-Element-in-an-Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int num : nums){
+            pq.offer(num);
+        }
+        for(int i=0;i<k-1;i++){
+            pq.poll();
+        }
+        return pq.poll();
+    }
+}
